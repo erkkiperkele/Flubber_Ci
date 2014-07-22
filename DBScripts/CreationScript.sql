46a82ea1d6bbd5ae5782ee05733dcf29ab380ba1
erkkiperkele <aymeric.grail@gmail.com>
2014-07-22 09:21:08 -0400

    Added a folder for the DB script
    I guess from now on, we could have an Update Script so we dn t recreate
    the whole database every time. Let me know what you think.

---
+++ DBScripts/CreationScript.sql
@@ -0,0 +1,253 @@
+/* Drop events/trigger if they already exist */
+
+drop event if exists incrementHearts;
+
+/* Drop tables if they already exist */
+
+drop table if exists MemberOfGiftExchange;
+drop table if exists MemberOfGroup;
+drop table if exists Related;
+drop table if exists Message;
+drop table if exists Request;
+drop table if exists Blocked;
+
+drop table if exists GiftExchange;
+drop table if exists GroupContent;
+drop table if exists Groups;
+
+drop table if exists PublicContent;
+
+drop table if exists Interest;
+drop table if exists WallContent;
+drop table if exists Gift;
+
+drop table if exists GiftType;
+
+drop table if exists Permission;
+
+drop table if exists InterestType;
+drop table if exists RelationshipType;
+drop table if exists Member;
+
+/* Re-create the tables anew */
+
+CREATE TABLE Member (
+	memberId INT AUTO_INCREMENT,
+	firstName VARCHAR(35) NOT NULL,
+	lastName VARCHAR(35) NOT NULL,
+	email VARCHAR(50) NOT NULL UNIQUE,
+	hashedPassword VARCHAR(255) NOT NULL,
+	profession VARCHAR(30),
+	address VARCHAR(40),
+	city VARCHAR(20),
+	country VARCHAR(20),
+	photographURL NVARCHAR(255),
+	coverPictureURL NVARCHAR(255),
+	thumbnailURL NVARCHAR(255),
+	hearts INT NOT NULL,
+	dateOfBirth DATE NOT NULL,
+	privacy VARCHAR(7) NOT NULL,
+	privilege VARCHAR(13) NOT NULL,
+	status VARCHAR(9) NOT NULL,
+	PRIMARY KEY(memberId),
+	CONSTRAINT heartsPositive CHECK (hearts >= 0)
+);
+
+CREATE TABLE RelationshipType(
+	relationshipTypeId INT AUTO_INCREMENT,
+	description VARCHAR(20) UNIQUE NOT NULL,
+	PRIMARY KEY(relationshipTypeId)
+);
+
+CREATE TABLE InterestType(
+	interestTypeId INT AUTO_INCREMENT,
+	description VARCHAR(20) UNIQUE NOT NULL,
+	PRIMARY KEY(interestTypeId)
+);
+
+CREATE TABLE Permission(
+	permissionId INT AUTO_INCREMENT,
+	description VARCHAR(7) UNIQUE NOT NULL,
+	PRIMARY KEY(permissionId)
+);
+
+CREATE TABLE GiftType(
+	giftTypeId INT AUTO_INCREMENT,
+	description VARCHAR(20) UNIQUE NOT NULL,
+	photographURL VARCHAR(20),
+	cost INT NOT NULL,
+	PRIMARY KEY(giftTypeId),
+	CONSTRAINT costPositive CHECK (cost > 0)
+);
+
+CREATE TABLE Gift(
+	sentTo INT,
+	sentFrom INT,
+	giftNumber INT,
+	giftTypeId INT,
+	timeStamp TIMESTAMP NOT NULL default CURRENT_TIMESTAMP,
+	PRIMARY KEY(sentTo, sentFrom, giftNumber),
+	FOREIGN KEY(sentTo) REFERENCES Member(memberId) ON DELETE CASCADE,
+	FOREIGN KEY(sentFrom) REFERENCES Member(memberId) ON DELETE CASCADE,
+	FOREIGN KEY(giftTypeId) REFERENCES GiftType(giftTypeId) ON DELETE CASCADE
+);
+
+CREATE TABLE WallContent(
+	memberId INT,
+	wallContentNumber INT,
+	permissionId INT NOT NULL,
+	currentPosterId INT,
+	previousPosterId INT,
+	originalPosterId INT,
+	contentType VARCHAR(15),
+	content VARCHAR(255),
+	timeStamp TIMESTAMP NOT NULL default CURRENT_TIMESTAMP,
+	PRIMARY KEY(memberId, wallContentNumber),
+	FOREIGN KEY(permissionId) REFERENCES Permission(permissionId),
+	FOREIGN KEY(memberId) REFERENCES Member(memberId) ON DELETE CASCADE,
+	FOREIGN KEY(currentPosterId) REFERENCES Member(memberId) ON DELETE SET NULL,
+	FOREIGN KEY(previousPosterId) REFERENCES Member(memberId) ON DELETE SET NULL,
+	FOREIGN KEY(originalPosterId) REFERENCES Member(memberId) ON DELETE SET NULL
+);
+
+CREATE TABLE Interest(
+	memberId INT,
+	interestNumber INT,
+	interestTypeId INT,
+	title VARCHAR(35),
+	artist VARCHAR(35),
+	PRIMARY KEY(memberId, interestNumber),
+	FOREIGN KEY(memberId) REFERENCES Member(memberId) ON DELETE CASCADE,
+	FOREIGN KEY(interestTypeId) REFERENCES InterestType(interestTypeId)
+);
+
+CREATE TABLE PublicContent(
+	memberId INT,
+	publicContentNumber INT,
+	contentType VARCHAR(15),
+	content VARCHAR(255),
+	timeStamp TIMESTAMP NOT NULL default CURRENT_TIMESTAMP,
+	PRIMARY KEY(memberId, publicContentNumber),
+	FOREIGN KEY(memberId) REFERENCES Member(memberId) ON DELETE CASCADE
+);
+
+CREATE TABLE Groups(
+	groupId INT AUTO_INCREMENT,
+	groupName VARCHAR(35) UNIQUE NOT NULL,
+	ownerId INT NOT NULL,
+	description VARCHAR(255),
+	timeStamp TIMESTAMP NOT NULL default CURRENT_TIMESTAMP,
+	PRIMARY KEY(groupId),
+	FOREIGN KEY(ownerId) REFERENCES Member(memberId) ON DELETE CASCADE
+);
+
+CREATE TABLE GroupContent(
+	groupId INT,
+	groupContentNumber INT,
+	permissionId INT NOT NULL,
+	currentPosterId INT NOT NULL,
+	previousPosterId INT,
+	originalPosterId INT,
+	contentType VARCHAR(15),
+	content VARCHAR(255),
+	timeStamp TIMESTAMP NOT NULL default CURRENT_TIMESTAMP,
+	PRIMARY KEY(groupId, groupContentNumber),
+	FOREIGN KEY(groupId) REFERENCES Groups(groupId) ON DELETE CASCADE,
+	FOREIGN KEY(permissionId) REFERENCES Permission(permissionId),
+	-- FOREIGN KEY(wrote) REFERENCES Member(memberId) ON DELETE CASCADE,
+	FOREIGN KEY(currentPosterId) REFERENCES Member(memberId) ON DELETE CASCADE,
+	FOREIGN KEY(previousPosterId) REFERENCES Member(memberId) ON DELETE SET NULL,
+	FOREIGN KEY(originalPosterId) REFERENCES Member(memberId) ON DELETE SET NULL
+);
+
+CREATE TABLE GiftExchange(
+	groupId INT,
+	giftExchangeNumber INT,
+	giftExchangeName VARCHAR(35) NOT NULL,
+	status VARCHAR(6),
+	timeStamp TIMESTAMP NOT NULL default CURRENT_TIMESTAMP,
+	PRIMARY KEY(groupId, giftExchangeNumber),
+	FOREIGN KEY(groupId) REFERENCES Groups(groupId) ON DELETE CASCADE
+);
+
+CREATE TABLE Blocked(
+	blockerId INT,
+	blockedId INT,
+	PRIMARY KEY(blockerId, blockedId),
+	FOREIGN KEY(blockerId) REFERENCES Member(memberId) ON DELETE CASCADE,
+	FOREIGN KEY(blockedId) REFERENCES Member(memberId) ON DELETE CASCADE
+);
+
+CREATE TABLE Request(
+	sentTo INT,
+	sentFrom INT,
+	requestNumber INT,
+	requestType VARCHAR(15),
+	content VARCHAR(255),
+	timeStamp TIMESTAMP NOT NULL default CURRENT_TIMESTAMP,
+	PRIMARY KEY(sentTo, sentFrom, requestNumber),
+	FOREIGN KEY(sentTo) REFERENCES Member(memberId) ON DELETE CASCADE,
+	FOREIGN KEY(sentFrom) REFERENCES Member(memberId) ON DELETE CASCADE
+);
+
+CREATE TABLE Message(
+	sentTo INT,
+	sentFrom INT,
+	messageNumber INT,
+	content VARCHAR(255),
+	timeStamp TIMESTAMP NOT NULL default CURRENT_TIMESTAMP,
+	PRIMARY KEY(sentTo, sentFrom, messageNumber),
+	FOREIGN KEY(sentTo) REFERENCES Member(memberId) ON DELETE CASCADE,
+	FOREIGN KEY(sentFrom) REFERENCES Member(memberId) ON DELETE CASCADE
+);
+
+CREATE TABLE Related(
+	memberId INT NOT NULL,
+	relatedId INT NOT NULL,
+	relationshipTypeId INT NOT NULL,
+	timeStamp TIMESTAMP NOT NULL default CURRENT_TIMESTAMP,
+	PRIMARY KEY(memberId, relatedId),
+	FOREIGN KEY(memberId) REFERENCES Member(memberId) ON DELETE CASCADE,
+	FOREIGN KEY(relatedId) REFERENCES Member(memberId) ON DELETE CASCADE,
+	FOREIGN KEY(relationshipTypeId) REFERENCES RelationshipType(relationshipTypeId) ON DELETE CASCADE
+);
+
+CREATE TABLE MemberOfGroup(
+	memberId INT,
+	groupId INT,
+	timeStamp TIMESTAMP NOT NULL default CURRENT_TIMESTAMP,
+	PRIMARY KEY(memberId, groupId),
+	FOREIGN KEY(memberId) REFERENCES Member(memberId),
+	FOREIGN KEY(groupId) REFERENCES Groups(groupId)
+);
+
+CREATE TABLE MemberOfGiftExchange(
+	groupId INT,
+	giftExchangeNumber INT,
+	memberId INT,
+	giftTypeId INT,
+	PRIMARY KEY(groupId, giftExchangeNumber, memberId),
+	FOREIGN KEY(groupId, giftExchangeNumber) REFERENCES GiftExchange(groupId, giftExchangeNumber) ON DELETE CASCADE,
+	FOREIGN KEY(memberId) REFERENCES Member(memberId) ON DELETE CASCADE,
+	FOREIGN KEY(giftTypeId) REFERENCES GiftType(giftTypeId)
+);
+
+/* Add events and triggers */
+
+CREATE EVENT incrementHearts
+ON SCHEDULE EVERY 1 DAY
+DO UPDATE Member SET hearts = hearts + 100;
+
+/* Add data members */
+
+INSERT INTO Permission(description)
+VALUES ('view'), ('comment'), ('link');
+
+INSERT INTO GiftType(description, photographURL, cost)
+VALUES ('Palm Tree', NULL, 10), ('Chocolate Cake', NULL, 15), ('Kitten', NULL, 35), ('Pony', NULL, 150);
+
+INSERT INTO RelationshipType(description)
+VALUES ('Family'), ('Friend'), ('Colleague');
+
+INSERT INTO InterestType(description)
+VALUES ('Music'), ('Movies'), ('Books'), ('Paintings');
---
+++ DBScripts/populating.sql
@@ -0,0 +1,45 @@
+select * from PublicContent;
+select * from Member;
+select * from wallContent;
+
+INSERT into Member
+	(memberId, firstName, LastName, Email, hashedPassword, profession, hearts, DateOfBirth, privacy, privilege, status, photographURL, coverPictureURL, thumbnailURL)
+	VALUES 
+	(1, 'Aymeric', 'Grail', 'a@aécom', '11', 'dev', 0, CURRENT_TIMESTAMP, 1, 1, 1, 'https://fbcdn-profile-a.akamaihd.net/hprofile-ak-xfa1/t1.0-1/c51.0.160.160/p160x160/1800265_10151856373511364_1778780040_n.jpg','https://scontent-a-lga.xx.fbcdn.net/hphotos-xaf1/t31.0-8/c0.254.851.315/p851x315/1782352_10151860443466364_254764027_o.jpg','https://fbcdn-profile-a.akamaihd.net/hprofile-ak-xfa1/t1.0-1/c16.0.50.50/p50x50/1800265_10151856373511364_1778780040_n.jpg'), 
+	(2, 'Sohail', 'Hooda', 'b@aécom', '11', 'dev', 0, CURRENT_TIMESTAMP, 1, 1, 1, 'https://fbcdn-profile-a.akamaihd.net/hprofile-ak-xfa1/t1.0-1/c37.37.466.466/s160x160/262882_10150259758063171_5182177_n.jpg','https://lh6.googleusercontent.com/-5vG8ole8nAI/UYFKqb0Y7YI/AAAAAAAABiA/YQzKopOzN1g/w1600-h900/default_cover_1_c07bbaef481e775be41b71cecbb5cd60.jpg','https://fbcdn-profile-a.akamaihd.net/hprofile-ak-xfa1/t1.0-1/c4.4.48.48/p56x56/262882_10150259758063171_5182177_n.jpg');
+
+INSERT into PublicContent 
+	(memberId, publicContentNumber, contentType, content, `timeStamp`)
+	VALUES
+	(1, 1, 1, 'my first content', CURRENT_TIMESTAMP), 
+	(2, 2, 1, 'Sohail first content', CURRENT_TIMESTAMP);
+
+
+INSERT into WallContent 
+	(memberId, wallContentNumber, permissionId, currentPosterId, previousPosterId, originalPosterID, contentType, content, `timeStamp`)
+	VALUES
+	 (1, 2, 1, 2, 2, 2, 1, 'Sohail first post on Aymeric\'s wall', '2014-07-10 12:39:41')
+	,(1, 1, 1, 1, 1, 1, 1, 'my first wall content', '2014-07-11 12:39:41')
+	,(2, 3, 1, 1, 1, 1, 1, 'Sohail first wall content', '2014-07-12 16:55:19')
+	,(1, 4, 1, 2, 2, 2, 1, 'Sohail second post on Aymeric\'s wall', '2014-07-13 16:55:19')
+	,(1, 5, 1, 2, 2, 2, 1, 'Sohail 3rd post on Aymeric\'s wall', '2014-07-12 14:55:19')
+	,(1, 6, 1, 2, 2, 2, 1, 'Sohail 4th post on Aymeric\'s wall', '2014-07-12 15:55:19')
+	,(1, 7, 1, 2, 2, 2, 1, 'Sohail 5th post on Aymeric\'s wall', '2014-07-12 16:55:19')
+	,(1, 8, 1, 2, 2, 2, 1, 'Sohail 6th post on Aymeric\'s wall', '2014-07-12 11:55:19')
+	,(1, 9, 1, 1, 1, 1, 1, 'my 2nd wall content', '2014-07-12 16:55:19')
+	,(1, 10, 1, 1, 1, 1, 1, 'my 3rd wall content', '2014-07-13 16:55:19')
+	;
+
+
+INSERT into Interest
+	(memberId, interestNumber, InterestTypeId, title, artist)
+	VALUES
+	(1, 1, 1, 'Nevermind', 'Nirvana'),
+	(1, 2, 2, 'Fight Club', 'David Fincher'),
+	(1, 3, 3, 'The hitchikers guide to the galaxy', 'Douglas Adams'),
+	(1, 4, 4, 'Bercement silencieux', 'Paul-Émile Borduas'),
+
+	(1, 5, 1, 'Modern Rhapsodies', 'Maxence Cyrin'),
+	(1, 6, 2, 'Django', 'Tarantino'),
+	(1, 7, 3, 'Le petit prince', 'St-Exupéry'),
+	(1, 8, 4, 'Guernica', 'Picasso');
