d817d7a033c2ccb2affab3c530bec8ed60d73dda
solo754 <sohail.hooda@hotmail.com>
2014-08-10 19:35:36 -0400

    Fixed "no search result" displaying

--- application/controllers/search.php
+++ application/controllers/search.php
@@ -11,8 +11,6 @@ class search extends FL_Controller {
 		if(!isset($query) || empty($query))
 			redirect("/profile/");
 		$result = $this->search_model->do_search($user, $query);
-		if(!isset($result) || empty($result))
-			redirect("/profile/");
 		$data['currentPage'] = 'search';
 		$data['title'] = 'Flubber - Search Results for ' .$query;
 		$data['member'] = $this->session->all_userdata();
@@ -21,12 +19,15 @@ class search extends FL_Controller {
 		$family = $this->search_model->get_relations($user, "family");
 		$colleagues = $this->search_model->get_relations($user, "colleagues");
 		//Connect all the relations of the member to the members in the search query results
-		if(!empty($friends))
-			$result = $this->search_model->join_relations_with_search_members($friends, "friend", $result);
-		if(!empty($family))
-			$result = $this->search_model->join_relations_with_search_members($family, "family", $result);
-		if(!empty($colleagues))
-			$result = $this->search_model->join_relations_with_search_members($colleagues, "colleague", $result);
+		if(!empty($result))
+		{
+			if(!empty($friends))
+				$result = $this->search_model->join_relations_with_search_members($friends, "friend", $result);
+			if(!empty($family))
+				$result = $this->search_model->join_relations_with_search_members($family, "family", $result);
+			if(!empty($colleagues))
+				$result = $this->search_model->join_relations_with_search_members($colleagues, "colleague", $result);
+		}
 		$data['result'] = $result;
 
 		$this->render('search', $data);
