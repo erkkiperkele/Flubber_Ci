7f90bcb5993e977c375b177e22ddf8bf29302348
solo754 <sohail.hooda@hotmail.com>
2014-08-10 17:26:13 -0400

    Bug fix for no search results found!

--- application/views/search_view.php
+++ application/views/search_view.php
@@ -3,8 +3,12 @@
 	<div class="panel interests panel-default">
 	  <div class="panel-heading text-center"><h1>Member Results</h1></div>
   		<div class="panel-body">
-  			<?php foreach ($result[0] as $member)
-				echo SearchResultMember($member);
+  			<?php if(is_array($result) && !empty($result[0]))
+  			{
+  				foreach ($result[0] as $member)
+					echo SearchResultMember($member);
+  			} else
+  				echo "<p>No results found!</p>";
 			?>
 		</div>
 	</div>
@@ -12,8 +16,12 @@
 	<div class="panel interests panel-default">
 	  <div class="panel-heading text-center"><h1>Group Results</h1></div>
   		<div class="panel-body">
-		  	<?php foreach ($result[1] as $group) 
-				echo SearchResultGroup($group);
+		  	<?php if(is_array($result) && !empty($result[1]))
+  			{
+				foreach ($result[1] as $group) 
+					echo SearchResultGroup($group);
+			}else
+				echo "<p>No results found!</p>";
 			?>
 		</div>
 	</div>
